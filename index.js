3a83c40c1f2b1d2b5448ea518f898bce4d975a25
Omar Ahmed <manoomarfcb2000@gmail.com>
2025-10-25 17:50:29 +0300

    for sharing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const postsRoutes = require("./routers/posts.route.js");
 const todosRoutes = require("./routers/todos.route.js");
 const authRoutes = require("./routers/auth.route.js");
+// const uploadRoutes = require("./routers/uploadpic.route.js");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 dotenv.config();
@@ -11,9 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: "http://localhost:3001", // your frontend
+  origin: "http://localhost:3001",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true, // ✅ allow cookies and auth headers
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -23,12 +24,13 @@ app.use(cookieParser());
 console.log("MONGO_URL:", process.env.MONGO_URL);
 mongoose
   .connect(`${process.env.MONGO_URL}/${process.env.DB_NAME}`)
-  .then(() => console.log("✅ Connected to MongoDB Atlas"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/posts", postsRoutes);
 app.use("/todos", todosRoutes);
 app.use("/auth", authRoutes);
+// app.use("/upload", uploadRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
--- models/users.js
+++ models/users.js
@@ -6,13 +6,23 @@ const userSchema = new Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
+  profilePicUrl: String,
   accountType: {
     type: String,
-    enum: ["investor", "project owner", "admin"],
+    enum: ["investor", "owner", "admin"],
     default: "investor",
     required: true,
   },
   createdAt: { type: Date, default: Date.now },
+  ownedProjects: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+  investedProjects: [
+    {
+      project: { type: Schema.Types.ObjectId, ref: "Project" },
+      percentage: { type: Number, required: true },
+      amount: { type: Number, required: true },
+      investedAt: { type: Date, default: Date.now },
+    },
+  ],
 });
 const User = mongoose.model("User", userSchema);
 module.exports = User;
