f989d8e6d3fa7c17b66ce8e16eac9db472067d24
Ibrahimabosharaa <ibrahimaboshaera@gmail.com>
2025-10-30 11:23:24 +0300

    edit cors

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: "http://localhost:3001",
+  origin: "*", 
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
 };
+app.use(cors(corsOptions));
+
 
 app.use(cors(corsOptions));
 app.use(express.json());
